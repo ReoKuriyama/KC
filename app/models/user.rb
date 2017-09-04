dafdf42712c80ede0a2a1db4010c2511e9e24681
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-09-04 19:45:24 +0900

    create group&member model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #Association
+  has_many :members
+  has_many :users, through: :members
 end
