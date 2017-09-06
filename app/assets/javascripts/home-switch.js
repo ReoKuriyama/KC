2c918690588cdfaea256abae04e21bbd45d70634
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-09-06 13:56:02 +0900

    fix

--- app/assets/javascripts/home-switch.js
+++ app/assets/javascripts/home-switch.js
@@ -2,7 +2,6 @@ $(function(){
   $(".tab-list li i").on('click', function(){
     $('.tab-content').css('display', 'none');
     var id = this.getAttribute("id");
-    var currentId = ("#" + id + "-content");
-    $(currentId).css("display", "block")
+    $("#" + id + "-content").css("display", "block")
   });
 });
