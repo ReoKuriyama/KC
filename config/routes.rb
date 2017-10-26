8104a23917e37f76fbbe3899537f01369956c94d
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-10-26 21:31:14 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   devise_for :users
-  resources :groups, only: %i(edit, update) do
+  resources :groups, only: %i(create edit update) do
     resources :messages, only: %i(index create)
   end
   resources :users, only: %i(edit update)
