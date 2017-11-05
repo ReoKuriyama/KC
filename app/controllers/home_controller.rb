34203f0bf0c647480d32d0b3a23cd56bdb894004
Reo Kuriyama <gagabornthisway34@gmail.com>
2017-11-05 19:00:44 +0900

    modify view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @user = current_user
     @users = User.all
     @groups = current_user.groups.dm.includes(:messages)
     @friends = current_user.friends
